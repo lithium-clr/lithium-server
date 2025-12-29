2f1b173e175cd8c025612ed3eb2e5a53e72b8804
Bubbies <66354328+bub-bl@users.noreply.github.com>
2025-12-29 17:34:31 +0100

    Create dog, cat entities and add position, rotation components on them

--- Lithium.Server.Core/Game.cs
+++ Lithium.Server.Core/Game.cs
@@ -4,11 +4,20 @@ namespace Lithium.Server.Core;
 
 public class Game
 {
-    public World World { get; private set; }
+    public World World { get; }
 
     public Game()
     {
         World = new World();
+        
+        var dog = World.CreateEntity();
+        var cat = World.CreateEntity();
+        
+        World.AddComponent(dog, new Position(0, 0, 0));
+        World.AddComponent(dog, new Rotation(0, 0, 0));
+        
+        World.AddComponent(cat, new Position(100, 100, 100));
+        World.AddComponent(cat, new Rotation(100, 100, 100));
         
         foreach (var (entity, position, rotation) in World.Query<Position, Rotation>())
         {
