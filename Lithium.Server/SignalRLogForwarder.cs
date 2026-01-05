c10f2e62c5ea3369d7933574e8e3ea4726a1eb2a
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-05 17:58:10 +0100

    Allow server to execute remote command. Add few signlar utilities to get commands from dashboard

---
+++ Lithium.Server/SignalRLogForwarder.cs
@@ -0,0 +1,32 @@
+using Lithium.Server.Dashboard;
+using Microsoft.AspNetCore.SignalR;
+using Serilog.Events;
+
+namespace Lithium.Server;
+
+public sealed class SignalRLogForwarder(
+    IHubContext<ServerConsoleHub, IServerConsoleHub> hubContext
+) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        SignalRSink.LogEmitted += OnLogEmitted;
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        SignalRSink.LogEmitted -= OnLogEmitted;
+        return Task.CompletedTask;
+    }
+
+    private async void OnLogEmitted(LogEvent logEvent)
+    {
+        var message = logEvent.RenderMessage();
+        
+        if (logEvent.Exception is not null)
+            message += Environment.NewLine + logEvent.Exception;
+
+        await hubContext.Clients.All.ReceiveLog(logEvent.Timestamp, (int)logEvent.Level, message);
+    }
+}
---
+++ Lithium.Server/SignalRSink.cs
@@ -0,0 +1,14 @@
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Lithium.Server;
+
+public sealed class SignalRSink : ILogEventSink
+{
+    public static event Action<LogEvent>? LogEmitted;
+
+    public void Emit(LogEvent logEvent)
+    {
+        LogEmitted?.Invoke(logEvent);
+    }
+}
