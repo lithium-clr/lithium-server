4350714d481cf4ab6db28e014f0717ab47063bda
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-12 23:45:36 +0100

    Remove shit comment

--- src/Lithium.Snowflake/Services/SnowflakeGenerator.cs
+++ src/Lithium.Snowflake/Services/SnowflakeGenerator.cs
@@ -8,10 +8,7 @@ public sealed class SnowflakeGenerator : IIdGenerator
 
     public SnowflakeGenerator(int generatorId = 0)
     {
-        // Epoch custom (ex: 1er Janvier 2024) pour maximiser la durée de vie des IDs
         var epoch = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        
-        // Standard Twitter Snowflake structure
         var structure = new IdStructure(41, 10, 12);
         var options = new IdGeneratorOptions(structure, new DefaultTimeSource(epoch));
         
