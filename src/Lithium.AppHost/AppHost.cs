862ad92b8d48c3e3bdb192915ace06a40249e795
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-13 12:27:02 +0100

    Setup dashboard in aspire

--- src/Lithium.AppHost/AppHost.cs
+++ src/Lithium.AppHost/AppHost.cs
@@ -7,4 +7,7 @@ var server = builder.AddProject<Projects.Lithium_Server>("server");
 builder.AddProject<Projects.Lithium_Client>("client")
     .WithReference(server);
 
+builder.AddProject<Projects.Lithium_Server_Dashboard>("dashboard")
+    .WithReference(server);
+
 builder.Build().Run();
