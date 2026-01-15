ddbcff0283afe1413f0bf4d5b8e1d7006e7b9c67
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-15 22:36:27 +0100

    Create AuthorizationGrantRequest class

---
+++ src/Lithium.Server.Core/Auth/AuthorizationGrantRequest.cs
@@ -0,0 +1,9 @@
+using System.Text.Json.Serialization;
+
+namespace Lithium.Server.Core.Auth;
+
+public sealed class AuthorizationGrantRequest
+{
+    [JsonPropertyName("identityToken")] public required string IdentityToken { get; init; }
+    [JsonPropertyName("aud")] public required string Audience { get; init; }
+}
