518a8139bb8c2ceb49d33085cfd7478c65a3f965
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-16 09:19:26 +0100

    Create default implementation for IAuthCredentialStore

---
+++ src/Lithium.Server.Core/Auth/DefaultAuthCredentialStore.cs
@@ -0,0 +1,13 @@
+namespace Lithium.Server.Core.Auth;
+
+public sealed class DefaultAuthCredentialStore : IAuthCredentialStore
+{
+    public OAuthTokens Tokens { get; set; } = new(null, null, null);
+    public Guid? ProfileId { get; set; }
+    
+    public void Clear()
+    {
+        Tokens = new OAuthTokens(null, null, null);
+        ProfileId = null;
+    }
+}
