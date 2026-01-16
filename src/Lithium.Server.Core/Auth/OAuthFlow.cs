ba980e8131a17385905e5d7c3c184ce90b7c42b2
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-16 10:34:19 +0100

    Create OAuthFlow abstract class. I not 100% sure about this implementation

---
+++ src/Lithium.Server.Core/Auth/OAuthFlow.cs
@@ -0,0 +1,34 @@
+using Lithium.Server.Core.Auth.OAuth;
+
+namespace Lithium.Server.Core.Auth;
+
+public abstract class OAuthFlow
+{
+    private readonly TaskCompletionSource<OAuthResult> _tcs =
+        new(TaskCreationOptions.RunContinuationsAsynchronously);
+    
+    public TokenResponse? TokenResponse { get; private set; }
+    public OAuthResult Result  { get; private set; } = OAuthResult.Unknown;
+    public string? ErrorMessage { get; private set; }
+    public Task<OAuthResult> Completion => _tcs.Task;
+    
+    public virtual void OnSuccess(TokenResponse tokenResponse)
+    {
+        if (_tcs.Task.IsCompleted) return;
+        
+        TokenResponse = tokenResponse;
+        Result = OAuthResult.Success;
+        
+        _tcs.TrySetResult(Result);
+    }
+
+    public virtual void OnFailure(string errorMessage)
+    {
+        if (_tcs.Task.IsCompleted) return;
+        
+        ErrorMessage = errorMessage;
+        Result = OAuthResult.Failed;
+        
+        _tcs.TrySetResult(Result);
+    }
+}
