0bec59bac72b899b853573dff295b722e5caf40e
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-18 00:28:38 +0100

    Remove dead code

--- src/Lithium.Server.Core/Auth/ICredentialStore.cs
+++
@@ -1,8 +0,0 @@
-namespace Lithium.Server.Core.Auth;
-
-public interface ICredentialStore
-{
-    void Clear();
-    Task SaveAsync(CancellationToken cancellationToken = default);
-    Task LoadAsync(CancellationToken cancellationToken = default);
-}
