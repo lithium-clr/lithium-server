a84b97a6d207ed28c3911d95d89154bda39069b3
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-18 21:14:32 +0100

    Add missing properties for IPacket. I dont know if we will use it, but seem to be needed

--- src/Lithium.Server.Core/Protocol/IPacket.cs
+++ src/Lithium.Server.Core/Protocol/IPacket.cs
@@ -6,6 +6,12 @@ public interface IPacket
 {
     static int PacketId { get; }
     static int ComputedSize { get; }
+    static int NullableBitFieldSize { get; }
+    static int FixedBlockSize { get; }
+    static int VariableFieldCount { get; }
+    static int VariableBlockStart { get; }
+    static int MaxSize { get; }
+    static bool IsCompressed { get; }
     
     void Serialize(IBufferWriter<byte> writer);
 }
