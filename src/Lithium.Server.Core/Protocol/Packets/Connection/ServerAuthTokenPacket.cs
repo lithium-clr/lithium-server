dc290407ef5114b7a8c3a3372639f797cc47475c
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-20 14:48:49 +0100

    ServerAuthToken packet can now be deserialized

--- src/Lithium.Server.Core/Protocol/Packets/Connection/ServerAuthTokenPacket.cs
+++ src/Lithium.Server.Core/Protocol/Packets/Connection/ServerAuthTokenPacket.cs
@@ -1,11 +1,11 @@
 namespace Lithium.Server.Core.Protocol.Packets.Connection;
 
-public readonly struct ServerAuthTokenPacket : IPacket<ServerAuthTokenPacket>
+public struct ServerAuthTokenPacket : IPacket<ServerAuthTokenPacket>
 {
     public static int Id => 13;
 
-    public string? ServerAccessToken { get; }
-    public byte[]? PasswordChallenge { get; }
+    public string? ServerAccessToken { get; private set; }
+    public byte[]? PasswordChallenge { get; private set; }
 
     public ServerAuthTokenPacket()
     {
@@ -16,18 +16,18 @@ public readonly struct ServerAuthTokenPacket : IPacket<ServerAuthTokenPacket>
         ServerAccessToken = serverAccessToken;
         PasswordChallenge = passwordChallenge;
     }
-    
+
     public void Serialize(Stream stream)
     {
         byte nullBits = 0;
-        
+
         if (ServerAccessToken is not null) nullBits |= 1;
         if (PasswordChallenge is not null) nullBits |= 2;
 
         stream.WriteByte(nullBits);
 
         using var varBlock = new MemoryStream();
-            
+
         var tokenOffset = -1;
         var challengeOffset = -1;
 
@@ -48,5 +48,37 @@ public readonly struct ServerAuthTokenPacket : IPacket<ServerAuthTokenPacket>
 
         varBlock.Position = 0;
         varBlock.CopyTo(stream);
+    }
+
+    public static ServerAuthTokenPacket Deserialize(byte[] buffer)
+    {
+        var obj = new ServerAuthTokenPacket();
+        var nullBits = buffer[0];
+        const int varBlockStart = 9;
+
+        if ((nullBits & 1) is not 0)
+        {
+            var tokenOffset = BitConverter.ToInt32(buffer, 1);
+
+            if (tokenOffset is not -1)
+            {
+                obj.ServerAccessToken = PacketSerializer.ReadVarString(buffer, varBlockStart + tokenOffset, out _);
+            }
+        }
+
+        if ((nullBits & 2) is not 0)
+        {
+            var challengeOffset = BitConverter.ToInt32(buffer, 5);
+
+            if (challengeOffset is not -1)
+            {
+                var len = PacketSerializer.ReadVarInt(buffer, varBlockStart + challengeOffset, out int varIntLen);
+                obj.PasswordChallenge = new byte[len];
+
+                Array.Copy(buffer, varBlockStart + challengeOffset + varIntLen, obj.PasswordChallenge, 0, len);
+            }
+        }
+
+        return obj;
     }
 }
