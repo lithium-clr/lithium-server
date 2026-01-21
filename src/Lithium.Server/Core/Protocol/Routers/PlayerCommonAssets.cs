b88680cc3da634187658eebe6bdcd24c9780ce97
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-21 23:22:40 +0100

    Create PlayerCommonAssets

---
+++ src/Lithium.Server/Core/Protocol/Routers/PlayerCommonAssets.cs
@@ -0,0 +1,35 @@
+using Lithium.Server.Core.Protocol.Packets;
+
+namespace Lithium.Server.Core.Protocol.Routers;
+
+public sealed class PlayerCommonAssets
+{
+    private readonly Dictionary<string, string> _assetsMissing = new();
+    private readonly Dictionary<string, string> _assetsSent = new();
+
+    public void Sent(IReadOnlyList<Asset> assets)
+    {
+        var set = new List<string>();
+
+        foreach (var asset in assets)
+            set.Add(asset.Hash);
+        
+        var keysToRemove = new List<string>();
+        
+        foreach (var hash in _assetsMissing.Keys)
+        {
+            if (!set.Contains(hash)) continue;
+            
+            keysToRemove.Add(hash);
+            set.Remove(hash);
+        }
+
+        if (set.Count is not 0)
+            throw new Exception("Still had hashes: " + set);
+       
+        foreach (var kvp in _assetsMissing) 
+            _assetsSent.Add(kvp.Key, kvp.Value);
+        
+        _assetsMissing.Clear();
+    }
+}
