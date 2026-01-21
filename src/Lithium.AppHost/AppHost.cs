e81bef71705cd59533af1ccebe4ae2fe14bbcb7d
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-21 23:27:49 +0100

    Remove client project reference from aspire

--- src/Lithium.AppHost/AppHost.cs
+++ src/Lithium.AppHost/AppHost.cs
@@ -4,9 +4,6 @@ builder.AddDockerComposeEnvironment("env");
 
 var server = builder.AddProject<Projects.Lithium_Server>("server");
 
-builder.AddProject<Projects.Lithium_Client>("client")
-    .WithReference(server);
-
 builder.AddProject<Projects.Lithium_Server_Dashboard>("dashboard")
     .WithReference(server);
 
