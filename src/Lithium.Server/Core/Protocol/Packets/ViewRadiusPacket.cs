21ee4a2f3649d3b533e4affff17ba1fd31f2aeca
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-21 23:20:00 +0100

    Create ViewRadiusPacket packet

---
+++ src/Lithium.Server/Core/Protocol/Packets/ViewRadiusPacket.cs
@@ -0,0 +1,25 @@
+using System.Buffers.Binary;
+
+namespace Lithium.Server.Core.Protocol.Packets;
+
+public readonly struct ViewRadiusPacket(int value) : IPacket<ViewRadiusPacket>
+{
+    public static int Id => 32;
+
+    public readonly int Value = value;
+
+    public static ViewRadiusPacket Deserialize(ReadOnlySpan<byte> buffer)
+    {
+        var reader = new PacketReader(buffer);
+        var value = reader.ReadInt32();
+
+        return new ViewRadiusPacket(value);
+    }
+
+    public void Serialize(Stream stream)
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        BinaryPrimitives.WriteInt32LittleEndian(buffer, Value);
+        stream.Write(buffer);
+    }
+}
