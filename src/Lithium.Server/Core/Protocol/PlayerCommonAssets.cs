191e3c28bcf8b4b4466352aa31ec2e579b144373
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-22 17:45:05 +0100

    Refactor PlayerCommonAssets asset handling

--- src/Lithium.Server/Core/Protocol/PlayerCommonAssets.cs
+++ src/Lithium.Server/Core/Protocol/PlayerCommonAssets.cs
@@ -5,31 +5,40 @@ namespace Lithium.Server.Core.Protocol;
 public sealed class PlayerCommonAssets
 {
     private readonly Dictionary<string, string> _assetsMissing = new();
-    private readonly Dictionary<string, string> _assetsSent = new();
+    private readonly Dictionary<string, string> _assetsSent = [];
 
-    public void Sent(IReadOnlyList<Asset> assets)
+    public void Initialize(Asset[] requiredAssets)
+    {
+        foreach (var requiredAsset in requiredAssets)
+            _assetsMissing.Add(requiredAsset.Hash, requiredAsset.Name);
+    }
+
+    public void Sent(Asset[]? assets)
     {
         var set = new List<string>();
 
-        foreach (var asset in assets)
-            set.Add(asset.Hash);
-        
+        if (assets is not null)
+        {
+            foreach (var asset in assets)
+                set.Add(asset.Hash);
+        }
+
         var keysToRemove = new List<string>();
-        
+
         foreach (var hash in _assetsMissing.Keys)
         {
             if (!set.Contains(hash)) continue;
-            
+
             keysToRemove.Add(hash);
             set.Remove(hash);
         }
 
         if (set.Count is not 0)
             throw new Exception("Still had hashes: " + set);
-       
-        foreach (var kvp in _assetsMissing) 
+
+        foreach (var kvp in _assetsMissing)
             _assetsSent.Add(kvp.Key, kvp.Value);
-        
+
         _assetsMissing.Clear();
     }
 }
