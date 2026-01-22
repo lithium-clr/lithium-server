b58e10ccf114d726c5b6e8f8ef0a10719e1f28b7
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-22 17:41:01 +0100

    Create PluginIdentifier

---
+++ src/Lithium.Server/Common/PluginIdentifier.cs
@@ -0,0 +1,18 @@
+namespace Lithium.Server.Core.Protocol;
+
+public sealed record PluginIdentifier(string Group, string Name)
+{
+    public static PluginIdentifier FromString(string str)
+    {
+        var parts = str.Split(':');
+
+        return parts.Length is not 2
+            ? throw new ArgumentException("Invalid plugin identifier")
+            : new PluginIdentifier(parts[0], parts[1]);
+    }
+
+    public override string ToString()
+    {
+        return Group + ':' + Name;
+    }
+}
