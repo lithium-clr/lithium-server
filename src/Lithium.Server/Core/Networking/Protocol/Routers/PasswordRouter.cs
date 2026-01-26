325d4c3e461351c5a81a7713f4b4c83acc5aaedf
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-27 00:18:33 +0100

    Override ShouldAcceptPacket for PasswordRouter

--- src/Lithium.Server/Core/Networking/Protocol/Routers/PasswordRouter.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Routers/PasswordRouter.cs
@@ -1,6 +1,14 @@
+using Lithium.Server.Core.Networking.Protocol.Packets;
+
 namespace Lithium.Server.Core.Networking.Protocol.Routers;
 
 public sealed partial class PasswordRouter(ILogger<PasswordRouter> logger, IPacketRegistry packetRegistry) : BasePacketRouter(logger, packetRegistry)
 {
     public override partial void Initialize(IServiceProvider sp);
+    
+    protected override bool ShouldAcceptPacket(INetworkConnection channel, int packetId, Packet packet) => packet switch
+    {
+        PasswordResponsePacket or DisconnectPacket  => true,
+        _ => false
+    };
 }
