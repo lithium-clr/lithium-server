4d2c75eafbe415e650d6e95593ede64606763e74
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-27 00:19:04 +0100

    Update ViewRadius packet infos

--- src/Lithium.Server/Core/Networking/Protocol/Packets/ViewRadiusPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/ViewRadiusPacket.cs
@@ -1,17 +1,18 @@
-// using Lithium.Server.Core.Protocol.Attributes;
-//
-// namespace Lithium.Server.Core.Networking.Protocol.Packets;
-//
-// [Packet(Id = 32, MaxSize = 12)]
-// public sealed class ViewRadiusPacket : Packet
-// {
-//     [PacketProperty(FixedIndex = 0)]
-//     public int Value { get; set; }
-//
-//     public ViewRadiusPacket() { }
-//
-//     public ViewRadiusPacket(int value)
-//     {
-//         Value = value;
-//     }
-// }
+using Lithium.Server.Core.Protocol.Attributes;
+
+namespace Lithium.Server.Core.Networking.Protocol.Packets;
+
+[Packet(Id = 32, IsCompressed = false, VariableBlockStart = 4, MaxSize = 4)]
+public sealed class ViewRadiusPacket : Packet
+{
+    // Java: value (fixed, offset 0)
+    [PacketProperty(FixedIndex = 0)]
+    public int Value { get; set; }
+
+    public ViewRadiusPacket() { }
+
+    public ViewRadiusPacket(int value)
+    {
+        Value = value;
+    }
+}
