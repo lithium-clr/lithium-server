8756ee385013913c7817cd9bf0de7f0efa4a244c
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-27 00:18:27 +0100

    Override ShouldAcceptPacket for HandshakeRouter

--- src/Lithium.Server/Core/Networking/Protocol/Routers/HandshakeRouter.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Routers/HandshakeRouter.cs
@@ -1,6 +1,15 @@
+using Lithium.Server.Core.Networking.Protocol.Packets;
+
 namespace Lithium.Server.Core.Networking.Protocol.Routers;
 
-public sealed partial class HandshakeRouter(ILogger<HandshakeRouter> logger, IPacketRegistry packetRegistry) : BasePacketRouter(logger, packetRegistry)
+public sealed partial class HandshakeRouter(ILogger<HandshakeRouter> logger, IPacketRegistry packetRegistry)
+    : BasePacketRouter(logger, packetRegistry)
 {
     public override partial void Initialize(IServiceProvider sp);
+
+    protected override bool ShouldAcceptPacket(INetworkConnection channel, int packetId, Packet packet) => packet switch
+    {
+        ConnectPacket or DisconnectPacket => true,
+        _ => false
+    };
 }
