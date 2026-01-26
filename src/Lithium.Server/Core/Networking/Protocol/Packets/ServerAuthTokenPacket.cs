c851b2ffda9316db87a355386a6a86d0bf9fa6f8
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 22:23:29 +0100

    Update ServerAuthToken packet infos

--- src/Lithium.Server/Core/Networking/Protocol/Packets/ServerAuthTokenPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/ServerAuthTokenPacket.cs
@@ -2,7 +2,7 @@ using Lithium.Server.Core.Protocol.Attributes;
 
 namespace Lithium.Server.Core.Networking.Protocol.Packets;
 
-[Packet(Id = 13)]
+[Packet(Id = 13, VariableBlockStart = 9, MaxSize = 32851)]
 public sealed class ServerAuthTokenPacket : Packet
 {
     [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
