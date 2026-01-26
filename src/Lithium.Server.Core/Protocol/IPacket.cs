de139add21379a2739cbb168d3673d0ce4144d47
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 14:10:20 +0100

    Refactor IPacket interface to use instance properties

--- src/Lithium.Server.Core/Protocol/IPacket.cs
+++ src/Lithium.Server.Core/Protocol/IPacket.cs
@@ -2,17 +2,21 @@ namespace Lithium.Server.Core.Protocol;
 
 public interface IPacket
 {
-    static abstract int Id { get; }
-    static virtual bool IsCompressed => false;
-    
-    void Serialize(Stream stream)
-    {
-        throw new NotImplementedException();
-    }
+    int Id { get; }
+    bool IsCompressed { get; }
+
+    void Serialize(Stream stream) => throw new NotImplementedException();
 }
 
 public interface IPacket<out T> : IPacket where T : IPacket<T>
 {
+    new static abstract int Id { get; }
+    new static virtual bool IsCompressed => false;
+
+    // Default Interface Methods (DIM) mapping static to instance
+    int IPacket.Id => T.Id;
+    bool IPacket.IsCompressed => T.IsCompressed;
+    
     static virtual T Deserialize(ReadOnlySpan<byte> buffer)
     {
         throw new NotImplementedException();
