7fd1e2bc38295bb92c860b31f91dd7977b5ec22a
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 22:23:38 +0100

    Update PasswordResponse packet infos

--- src/Lithium.Server/Core/Networking/Protocol/Packets/PasswordResponsePacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/PasswordResponsePacket.cs
@@ -2,9 +2,10 @@ using Lithium.Server.Core.Protocol.Attributes;
 
 namespace Lithium.Server.Core.Networking.Protocol.Packets;
 
-[Packet(Id = 15, VariableBlockStart = 5, MaxSize = 1024)]
+[Packet(Id = 15, VariableBlockStart = 1, MaxSize = 70)]
 public sealed class PasswordResponsePacket : Packet
 {
-    [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
+    // Java: hash (nullable, bit 0), no OffsetIndex (sequential)
+    [PacketProperty(BitIndex = 0)]
     public byte[]? Hash { get; set; }
 }
