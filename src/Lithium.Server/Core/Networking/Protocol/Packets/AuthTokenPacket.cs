bc2bbd3c0e8ac2be8a4f1dae7dd67eec0d8b3981
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 22:02:33 +0100

    Update AuthToken packet infos to match with the java packet

--- src/Lithium.Server/Core/Networking/Protocol/Packets/AuthTokenPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/AuthTokenPacket.cs
@@ -2,12 +2,14 @@ using Lithium.Server.Core.Protocol.Attributes;
 
 namespace Lithium.Server.Core.Networking.Protocol.Packets;
 
-[Packet(Id = 12, VariableBlockStart = 9, MaxSize = 16384)]
+[Packet(Id = 12, VariableBlockStart = 9, MaxSize = 49171)]
 public sealed class AuthTokenPacket : Packet
 {
+    // Java: accessToken (nullable, bit 0), OffsetIndex 0
     [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
     public string? AccessToken { get; set; }
 
+    // Java: serverAuthorizationGrant (nullable, bit 1), OffsetIndex 1
     [PacketProperty(BitIndex = 1, OffsetIndex = 1)]
     public string? ServerAuthorizationGrant { get; set; }
 }
