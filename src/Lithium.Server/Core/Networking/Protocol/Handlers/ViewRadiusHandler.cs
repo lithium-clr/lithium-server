a8473a4e4c4410df85ef7f3bdcb7c9a09471e344
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 23:48:06 +0100

    Ignore this code for now

--- src/Lithium.Server/Core/Networking/Protocol/Handlers/ViewRadiusHandler.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Handlers/ViewRadiusHandler.cs
@@ -1,26 +1,26 @@
-using Lithium.Server.Core.Networking.Protocol.Attributes;
-using Lithium.Server.Core.Networking.Protocol.Packets;
-using Lithium.Server.Core.Networking.Protocol.Routers;
-
-
-namespace Lithium.Server.Core.Networking.Protocol.Handlers;
-
-[RegisterPacketHandler(typeof(SetupPacketRouter))]
-public sealed class ViewRadiusHandler(
-    ILogger<ViewRadiusHandler> logger,
-    IClientManager clientManager
-) : IPacketHandler<ViewRadiusPacket>
-{
-    private float _clientViewRadiusChunks = 6f;
-    
-    public  Task Handle(INetworkConnection channel, ViewRadiusPacket packet)
-    {
-        var client = clientManager.GetClient(channel);
-        if (client is null) return Task.CompletedTask;
-        
-        _clientViewRadiusChunks = MathF.Ceiling(packet.Value / 32.0F);
-        client.ViewRadiusChunks = _clientViewRadiusChunks;
-
-        return Task.CompletedTask;
-    }
-}
+// using Lithium.Server.Core.Networking.Protocol.Attributes;
+// using Lithium.Server.Core.Networking.Protocol.Packets;
+// using Lithium.Server.Core.Networking.Protocol.Routers;
+//
+//
+// namespace Lithium.Server.Core.Networking.Protocol.Handlers;
+//
+// [RegisterPacketHandler(typeof(SetupPacketRouter))]
+// public sealed class ViewRadiusHandler(
+//     ILogger<ViewRadiusHandler> logger,
+//     IClientManager clientManager
+// ) : IPacketHandler<ViewRadiusPacket>
+// {
+//     private float _clientViewRadiusChunks = 6f;
+//     
+//     public  Task Handle(INetworkConnection channel, ViewRadiusPacket packet)
+//     {
+//         var client = clientManager.GetClient(channel);
+//         if (client is null) return Task.CompletedTask;
+//         
+//         _clientViewRadiusChunks = MathF.Ceiling(packet.Value / 32.0F);
+//         client.ViewRadiusChunks = _clientViewRadiusChunks;
+//
+//         return Task.CompletedTask;
+//     }
+// }
--- src/Lithium.Server/Core/Networking/Protocol/Packets/ViewRadiusPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/ViewRadiusPacket.cs
@@ -1,17 +1,17 @@
-using Lithium.Server.Core.Protocol.Attributes;
-
-namespace Lithium.Server.Core.Networking.Protocol.Packets;
-
-[Packet(Id = 32, MaxSize = 12)]
-public sealed class ViewRadiusPacket : Packet
-{
-    [PacketProperty(FixedIndex = 0)]
-    public int Value { get; set; }
-
-    public ViewRadiusPacket() { }
-
-    public ViewRadiusPacket(int value)
-    {
-        Value = value;
-    }
-}
+// using Lithium.Server.Core.Protocol.Attributes;
+//
+// namespace Lithium.Server.Core.Networking.Protocol.Packets;
+//
+// [Packet(Id = 32, MaxSize = 12)]
+// public sealed class ViewRadiusPacket : Packet
+// {
+//     [PacketProperty(FixedIndex = 0)]
+//     public int Value { get; set; }
+//
+//     public ViewRadiusPacket() { }
+//
+//     public ViewRadiusPacket(int value)
+//     {
+//         Value = value;
+//     }
+// }
