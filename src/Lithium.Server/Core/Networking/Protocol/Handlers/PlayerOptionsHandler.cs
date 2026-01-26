7ae830b6107d401f6525e23446a4508a07e6177d
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-26 23:43:49 +0100

    Ignore this code for now

--- src/Lithium.Server/Core/Networking/Protocol/Handlers/PlayerOptionsHandler.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Handlers/PlayerOptionsHandler.cs
@@ -1,35 +1,35 @@
-using Lithium.Server.Core.Networking.Authentication;
-using Lithium.Server.Core.Networking.Protocol.Attributes;
-using Lithium.Server.Core.Networking.Protocol.Packets;
-using Lithium.Server.Core.Networking.Protocol.Routers;
-
-
-namespace Lithium.Server.Core.Networking.Protocol.Handlers;
-
-[RegisterPacketHandler(typeof(SetupPacketRouter))]
-public sealed class PlayerOptionsHandler(
-    ILogger<PlayerOptionsHandler> logger,
-    IServerAuthManager serverAuthManager,
-    ISessionServiceClient sessionServiceClient,
-    IClientManager clientManager,
-    IServerManager serverManager,
-    JwtValidator jwtValidator,
-    PacketRouterService routerService
-) : IPacketHandler<PlayerOptionsPacket>
-{
-    public  Task Handle(INetworkConnection channel, PlayerOptionsPacket packet)
-    {
-        var client = clientManager.GetClient(channel);
-        if (client is null) return Task.CompletedTask;
-
-        if (client.IsActive)
-        {
-            if (packet.Skin is not null)
-            {
-                
-            }
-        }
-
-        return Task.CompletedTask;
-    }
-}
+// using Lithium.Server.Core.Networking.Authentication;
+// using Lithium.Server.Core.Networking.Protocol.Attributes;
+// using Lithium.Server.Core.Networking.Protocol.Packets;
+// using Lithium.Server.Core.Networking.Protocol.Routers;
+//
+//
+// namespace Lithium.Server.Core.Networking.Protocol.Handlers;
+//
+// [RegisterPacketHandler(typeof(SetupPacketRouter))]
+// public sealed class PlayerOptionsHandler(
+//     ILogger<PlayerOptionsHandler> logger,
+//     IServerAuthManager serverAuthManager,
+//     ISessionServiceClient sessionServiceClient,
+//     IClientManager clientManager,
+//     IServerManager serverManager,
+//     JwtValidator jwtValidator,
+//     PacketRouterService routerService
+// ) : IPacketHandler<PlayerOptionsPacket>
+// {
+//     public  Task Handle(INetworkConnection channel, PlayerOptionsPacket packet)
+//     {
+//         var client = clientManager.GetClient(channel);
+//         if (client is null) return Task.CompletedTask;
+//
+//         if (client.IsActive)
+//         {
+//             if (packet.Skin is not null)
+//             {
+//                 
+//             }
+//         }
+//
+//         return Task.CompletedTask;
+//     }
+// }
--- src/Lithium.Server/Core/Networking/Protocol/Packets/PlayerOptionsPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/PlayerOptionsPacket.cs
@@ -1,10 +1,10 @@
-using Lithium.Server.Core.Protocol.Attributes;
-
-namespace Lithium.Server.Core.Networking.Protocol.Packets;
-
-[Packet(Id = 33, VariableBlockStart = 5, MaxSize = 32768)]
-public sealed class PlayerOptionsPacket : Packet
-{
-    [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
-    public PlayerSkin? Skin { get; set; }
-}
+// using Lithium.Server.Core.Protocol.Attributes;
+//
+// namespace Lithium.Server.Core.Networking.Protocol.Packets;
+//
+// [Packet(Id = 33, VariableBlockStart = 5, MaxSize = 32768)]
+// public sealed class PlayerOptionsPacket : Packet
+// {
+//     [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
+//     public PlayerSkin? Skin { get; set; }
+// }
