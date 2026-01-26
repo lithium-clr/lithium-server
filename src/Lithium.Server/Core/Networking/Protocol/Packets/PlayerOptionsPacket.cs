08b1385bf7b28167b8ed8af086ec87f80565f65a
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-27 00:21:16 +0100

    Update PlayerOptions packet infos

--- src/Lithium.Server/Core/Networking/Protocol/Packets/PlayerOptionsPacket.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/PlayerOptionsPacket.cs
@@ -1,10 +1,11 @@
-// using Lithium.Server.Core.Protocol.Attributes;
-//
-// namespace Lithium.Server.Core.Networking.Protocol.Packets;
-//
-// [Packet(Id = 33, VariableBlockStart = 5, MaxSize = 32768)]
-// public sealed class PlayerOptionsPacket : Packet
-// {
-//     [PacketProperty(BitIndex = 0, OffsetIndex = 0)]
-//     public PlayerSkin? Skin { get; set; }
-// }
+using Lithium.Server.Core.Protocol.Attributes;
+
+namespace Lithium.Server.Core.Networking.Protocol.Packets;
+
+[Packet(Id = 33, IsCompressed = false, VariableBlockStart = 1, MaxSize = 327680184)]
+public sealed class PlayerOptionsPacket : Packet
+{
+    // Java: skin (nullable, bit 0), no OffsetIndex (sequential)
+    [PacketProperty(BitIndex = 0)]
+    public PlayerSkin? Skin { get; set; }
+}
