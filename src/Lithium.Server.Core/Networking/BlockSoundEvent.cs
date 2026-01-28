d8180fa2d3079c912240f6ec0608de8a7e05888d
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-28 13:29:20 +0100

    Create BlockSoundEvent enum

---
+++ src/Lithium.Server.Core/Networking/BlockSoundEvent.cs
@@ -0,0 +1,14 @@
+namespace Lithium.Server.Core.Networking;
+
+public enum BlockSoundEvent : byte
+{
+    Walk = 0,
+    Land = 1,
+    MoveIn = 2,
+    MoveOut = 3,
+    Hit = 4,
+    Break = 5,
+    Build = 6,
+    Clone = 7,
+    Harvest = 8
+}
