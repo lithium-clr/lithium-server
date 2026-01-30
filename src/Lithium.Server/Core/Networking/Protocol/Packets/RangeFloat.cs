c8e776a5eb3c218e5a3b40765d5246e7363e03c7
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-30 17:14:50 +0100

    Create RangeFloat struct

---
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/RangeFloat.cs
@@ -0,0 +1,19 @@
+namespace Lithium.Server.Core.Networking.Protocol.Packets;
+
+public struct RangeFloat : INetworkSerializable
+{
+    public float Min { get; set; }
+    public float Max { get; set; }
+
+    public void Serialize(PacketWriter writer)
+    {
+        writer.WriteFloat32(Min);
+        writer.WriteFloat32(Max);
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        Min = reader.ReadFloat32();
+        Max = reader.ReadFloat32();
+    }
+}
