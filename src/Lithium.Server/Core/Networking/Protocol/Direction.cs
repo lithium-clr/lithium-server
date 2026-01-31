3c3adcff04732b00d33861fc9a45fb1e2a0b88c1
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-31 14:39:20 +0100

    Add missing json property name attribute on properties

--- src/Lithium.Server/Core/Networking/Protocol/Direction.cs
+++ src/Lithium.Server/Core/Networking/Protocol/Direction.cs
@@ -1,10 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Lithium.Server.Core.Networking.Protocol;
 
 public record struct Direction : INetworkSerializable
 {
-    public float Yaw { get; set; }
-    public float Pitch { get; set; }
-    public float Roll { get; set; }
+    [JsonPropertyName("yaw")] public float Yaw { get; set; }
+    [JsonPropertyName("pitch")] public float Pitch { get; set; }
+    [JsonPropertyName("roll")] public float Roll { get; set; }
 
     public void Serialize(PacketWriter writer)
     {
