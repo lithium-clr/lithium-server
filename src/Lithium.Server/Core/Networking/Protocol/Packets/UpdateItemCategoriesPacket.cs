80a2610269b0024287cd44e0f109714cad16f421
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-31 21:39:20 +0100

    Create UpdateItemCategoriesPacket

---
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/UpdateItemCategoriesPacket.cs
@@ -0,0 +1,55 @@
+using System.Text.Json.Serialization;
+using Lithium.Server.Core.Networking.Protocol.Attributes;
+
+namespace Lithium.Server.Core.Networking.Protocol.Packets;
+
+[Packet(
+    Id = 56,
+    IsCompressed = true,
+    NullableBitFieldSize = 1,
+    FixedBlockSize = 2,
+    VariableFieldCount = 1,
+    VariableBlockStart = 2,
+    MaxSize = 1677721600
+)]
+public sealed class UpdateItemCategoriesPacket : INetworkSerializable
+{
+    [JsonPropertyName("type")] public UpdateType Type { get; set; } = UpdateType.Init;
+    [JsonPropertyName("itemCategories")] public ItemCategory[]? ItemCategories { get; set; }
+
+    public void Serialize(PacketWriter writer)
+    {
+        var bits = new BitSet(1);
+
+        if (ItemCategories is not null) bits.SetBit(1);
+
+        writer.WriteBits(bits);
+        writer.WriteEnum(Type);
+
+        if (ItemCategories is not null)
+        {
+            writer.WriteVarInt(ItemCategories.Length);
+            foreach (var item in ItemCategories)
+            {
+                item.Serialize(writer);
+            }
+        }
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        var bits = reader.ReadBits();
+        Type = reader.ReadEnum<UpdateType>();
+
+        if (bits.IsSet(1))
+        {
+            var count = reader.ReadVarInt32();
+            ItemCategories = new ItemCategory[count];
+            for (var i = 0; i < count; i++)
+            {
+                ItemCategories[i] = new ItemCategory();
+                ItemCategories[i].Deserialize(reader);
+            }
+        }
+    }
+}
