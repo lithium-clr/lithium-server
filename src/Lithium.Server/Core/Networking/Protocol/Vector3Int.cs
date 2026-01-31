dba595514cc7b061b5cad2892775df156bab52a7
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-31 21:04:44 +0100

    Create basic implementation for Vector3Int

---
+++ src/Lithium.Server/Core/Networking/Protocol/Vector3Int.cs
@@ -0,0 +1,52 @@
+namespace Lithium.Server.Core.Networking.Protocol;
+
+public record struct Vector3Int : IVector<int>, INetworkSerializable
+{
+    public int X;
+    public int Y;
+    public int Z;
+    
+    public static Vector3Int Zero => new();
+    public static Vector3Int One => new();
+
+    public Vector3Int()
+    {
+    }
+    
+    public Vector3Int(int all)
+    {
+        X = Y = Z = all;
+    }
+    
+    public Vector3Int(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public void Serialize(PacketWriter writer)
+    {
+        writer.WriteInt32(X);
+        writer.WriteInt32(Y);
+        writer.WriteInt32(Z);
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        X = reader.ReadInt32();
+        Y = reader.ReadInt32();
+        Z = reader.ReadInt32();
+    }
+
+    public string ToString(string? format, IFormatProvider? formatProvider)
+    {
+        FormattableString formattable = $"";
+        return formattable.ToString(formatProvider);
+    }
+
+    public override string ToString()
+    {
+        return $"";
+    }
+}
