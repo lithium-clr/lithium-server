e0508b990f743255455b60a76770e810604cf316
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-31 14:03:43 +0100

    Create Tint struct

---
+++ src/Lithium.Server/Core/Networking/Protocol/Tint.cs
@@ -0,0 +1,31 @@
+namespace Lithium.Server.Core.Networking.Protocol;
+
+public record struct Tint : INetworkSerializable
+{
+    public int Top { get; set; }
+    public int Bottom { get; set; }
+    public int Front { get; set; }
+    public int Back { get; set; }
+    public int Left { get; set; }
+    public int Right { get; set; }
+
+    public void Serialize(PacketWriter writer)
+    {
+        writer.WriteInt32(Top);
+        writer.WriteInt32(Bottom);
+        writer.WriteInt32(Front);
+        writer.WriteInt32(Back);
+        writer.WriteInt32(Left);
+        writer.WriteInt32(Right);
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        Top = reader.ReadInt32();
+        Bottom = reader.ReadInt32();
+        Front = reader.ReadInt32();
+        Back = reader.ReadInt32();
+        Left = reader.ReadInt32();
+        Right = reader.ReadInt32();
+    }
+}
