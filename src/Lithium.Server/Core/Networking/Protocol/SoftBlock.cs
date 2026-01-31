2361674c4b6627abf21eadc42176c287afed5ab3
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-31 14:38:28 +0100

    Create SoftBlock

---
+++ src/Lithium.Server/Core/Networking/Protocol/SoftBlock.cs
@@ -0,0 +1,76 @@
+using System.Text.Json.Serialization;
+using Lithium.Server.Core.Networking.Protocol.Attributes;
+
+namespace Lithium.Server.Core.Networking.Protocol;
+
+[Packet(
+    NullableBitFieldSize = 1,
+    FixedBlockSize = 2,
+    VariableFieldCount = 2,
+    VariableBlockStart = 10,
+    MaxSize = 32768020
+)]
+public sealed class SoftBlock : INetworkSerializable
+{
+    [JsonPropertyName("itemId")] public string? ItemId { get; set; }
+    [JsonPropertyName("dropListId")] public string? DropListId { get; set; }
+
+    [JsonPropertyName("isWeaponBreakable")]
+    public bool IsWeaponBreakable { get; set; }
+
+    public void Serialize(PacketWriter writer)
+    {
+        var bits = new BitSet(1);
+
+        if (ItemId is not null) bits.SetBit(1);
+        if (DropListId is not null) bits.SetBit(2);
+
+        writer.WriteBits(bits);
+
+        writer.WriteBoolean(IsWeaponBreakable);
+
+        var itemIdOffsetSlot = writer.ReserveOffset();
+        var dropListIdOffsetSlot = writer.ReserveOffset();
+
+        var varBlockStart = writer.Position;
+
+        if (ItemId is not null)
+        {
+            writer.WriteOffsetAt(itemIdOffsetSlot, writer.Position - varBlockStart);
+            writer.WriteVarUtf8String(ItemId, 4096000);
+        }
+        else
+        {
+            writer.WriteOffsetAt(itemIdOffsetSlot, -1);
+        }
+
+        if (DropListId is not null)
+        {
+            writer.WriteOffsetAt(dropListIdOffsetSlot, writer.Position - varBlockStart);
+            writer.WriteVarUtf8String(DropListId, 4096000);
+        }
+        else
+        {
+            writer.WriteOffsetAt(dropListIdOffsetSlot, -1);
+        }
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        var bits = reader.ReadBits();
+
+        IsWeaponBreakable = reader.ReadBoolean();
+
+        var offsets = reader.ReadOffsets(2);
+
+        if (bits.IsSet(1))
+        {
+            ItemId = reader.ReadVarUtf8StringAt(offsets[0]);
+        }
+
+        if (bits.IsSet(2))
+        {
+            DropListId = reader.ReadVarUtf8StringAt(offsets[1]);
+        }
+    }
+}
