2de3d810965f96dd670a3899af32349742cfae2f
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-02-01 10:48:54 +0100

    Create UpdateAmbienceFxPacket

---
+++ src/Lithium.Server/Core/Networking/Protocol/Packets/UpdateAmbienceFxPacket.cs
@@ -0,0 +1,60 @@
+using Lithium.Server.Core.Networking.Protocol.Attributes;
+
+namespace Lithium.Server.Core.Networking.Protocol.Packets;
+
+[Packet(
+    Id = 62,
+    IsCompressed = true,
+    NullableBitFieldSize = 1,
+    FixedBlockSize = 6,
+    VariableFieldCount = 1,
+    VariableBlockStart = 6,
+    MaxSize = 1677721600
+)]
+public sealed class UpdateAmbienceFxPacket : INetworkSerializable
+{
+    public UpdateType Type { get; set; } = UpdateType.Init;
+    public int MaxId { get; set; }
+    public Dictionary<int, AmbienceFx>? AmbienceFx { get; set; }
+
+    public void Serialize(PacketWriter writer)
+    {
+        var bits = new BitSet(1);
+
+        if (AmbienceFx is not null) bits.SetBit(1);
+
+        writer.WriteBits(bits);
+        writer.WriteEnum(Type);
+        writer.WriteInt32(MaxId);
+
+        if (AmbienceFx is not null)
+        {
+            writer.WriteVarInt(AmbienceFx.Count);
+            foreach (var (key, value) in AmbienceFx)
+            {
+                writer.WriteInt32(key);
+                value.Serialize(writer);
+            }
+        }
+    }
+
+    public void Deserialize(PacketReader reader)
+    {
+        var bits = reader.ReadBits();
+        Type = reader.ReadEnum<UpdateType>();
+        MaxId = reader.ReadInt32();
+
+        if (bits.IsSet(1))
+        {
+            var count = reader.ReadVarInt32();
+            AmbienceFx = new Dictionary<int, AmbienceFx>(count);
+            for (var i = 0; i < count; i++)
+            {
+                var key = reader.ReadInt32();
+                var value = new AmbienceFx();
+                value.Deserialize(reader);
+                AmbienceFx[key] = value;
+            }
+        }
+    }
+}
